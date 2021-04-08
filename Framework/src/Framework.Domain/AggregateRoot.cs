a65a34717655cf1ea36e814b8a931ee790a3d37f
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2021-04-08 13:15:53 +0430

    initial commit

---
+++ Framework/src/Framework.Domain/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Domain
+{
+    public abstract class AggregateRoot<TKey> : Entity<TKey>
+    {
+
+    }
+}
